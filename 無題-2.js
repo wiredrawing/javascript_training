a2b59370e789d1f46c8f2695b0e6d851f6e6dfea
senbiki akifumi <akifumi.senbiki.1209@gmail.com>
2021-02-27 20:02:50 +0900

    for in ループ内で直接のプロパティのみをループで出力させるように修正

--- 無題-2.js
+++ 無題-2.js
@@ -1,6 +1,56 @@
+const { DirectiveLocation }=require( "graphql" );
+
+function Base()
+{
+    // インスタンス化に使ったコンストラクタ名
+    this.name = this.constructor.name;
+}
+
+Base.prototype.method = function () {
+    console.dir("プロトタイプメソッド");
+}
+Base.prototype.property = "プロトタイプメンバ";
 
 
-var EventEmitter = require("event").EventEmitter;
+function inherit(prototype) {
+    function F () {}
+    F.prototype = prototype;
+    var f = new F();
+    // f.__proto__ プロトタイプチェーン用のプロトタイプオブジェクトを持つ
+    return f;
+}
 
-var e1 = new EventEmitter(22);
-console.dir(e1);
+function Derived () {
+    Base.call(this);
+    console.dir(this);
+}
+var middle = inherit(Base.prototype);
+// middle => { __proto__: Base.prototype }
+Derived.prototype = middle;
+Derived.prototype.constructor = Derived;
+/*
+Derived.prototype => {
+    __proto__: Base.prototype,
+    constructor: Derived,
+}
+*/
+
+var derived = new Derived ();
+/*
+    derived = {
+        __proto__: {
+            __proto__: {
+
+            }
+        }
+    }
+*/
+
+console.dir(derived.hasOwnProperty("name"));
+console.dir(derived.__proto__ === Derived.prototype);
+console.dir(derived.__proto__.__proto__ === Base.prototype);
+for (var key in derived) {
+    if (derived.hasOwnProperty(key)) {
+        console.dir(key);
+    }
+}
